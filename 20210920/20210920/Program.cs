1e71834619f88769abc4a7cd8094e631af272aa2
majnekadam9955 <majnekadam9955@gmail.com>
2021-09-20 08:22:30 +0200

    feladat1

--- 20210920/20210920/Program.cs
+++ 20210920/20210920/Program.cs
@@ -8,10 +8,31 @@ namespace _20210920
 {
     class Program
     {
+
+        static void feladat1()
+        {
+
+            Console.WriteLine("kérek egy számot!");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+
+                Console.WriteLine("A szám páros!");
+
+            }
+            else
+            {
+
+                Console.WriteLine("A szám páratlam!");
+
+            }
+
+        }
+
         static void Main(string[] args)
         {
 
-
+            feladat1();
 
         }
     }
