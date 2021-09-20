27dc842dba4c29aaaeccb8c65d16766a9d0ada0c
majnekadam9955 <majnekadam9955@gmail.com>
2021-09-20 08:34:13 +0200

    1-3feladat

--- 20210920/20210920/Program.cs
+++ 20210920/20210920/Program.cs
@@ -29,11 +29,93 @@ namespace _20210920
 
         }
 
+        static void feladat2()
+        {
+
+            Console.WriteLine("kérek egy számot!");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a < 0)
+            {
+
+                Console.WriteLine("A szám negatív!");
+
+            }
+            else if (a == 0)
+            {
+
+                Console.WriteLine("A szám eggyenlő 0-val");
+
+            }
+            else
+            {
+
+                Console.WriteLine("a szám pozitív!");
+
+            }
+
+        }
+
+        static void feladat3()
+        {
+
+            Console.WriteLine("kérek egy számot!");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a < 0)
+            {
+
+                Console.WriteLine("Odakint fagy van!");
+
+            }
+            else if (a == 0)
+            {
+
+                Console.WriteLine("Fagypont van");
+
+            }
+            else
+            {
+
+                Console.WriteLine("Nincs fagy odakint");
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
         static void Main(string[] args)
         {
 
             feladat1();
 
+            feladat2();
+
+            feladat3();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("nyomjon enter-t!");
+            Console.ReadLine();
         }
     }
 }
