0ed7578115d8b412e6c34d49c05121b2f22533e8
majnekadam9955 <majnekadam9955@gmail.com>
2021-09-27 09:36:50 +0200

    1-5feladat kész

--- 20210927/20210927/Program.cs
+++ 20210927/20210927/Program.cs
@@ -73,9 +73,11 @@ namespace _20210927
                     break;
 
 
+
                 default:
                     Console.WriteLine("túl nagy számot adtál meg");
                     break;
+
             }
 
         }
@@ -151,8 +153,156 @@ namespace _20210927
             Console.WriteLine();
             Console.WriteLine("Kérek egy számot!");
 
+            //2 000 000 000
+            int szam = Convert.ToInt32(Console.ReadLine());
+            while (szam < 2000000000)
+            {
+
+                Console.WriteLine("nem jó számot adtál meg");
+                Console.WriteLine("kérek egy számot!");
+                szam = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            switch (Convert.ToString(szam).Length)
+            {
+
+                case 1:
+                    Console.WriteLine("egyjegyű");
+                    break;
+
+                case 2:
+                    Console.WriteLine("kétjegyű");
+                    break;
+
+                case 3:
+                    Console.WriteLine("háromjegyű");
+                    break;
+
+                case 4:
+                    Console.WriteLine("négyjegyű");
+                    break;
+
+                case 5:
+                    Console.WriteLine("ötjegyű");
+                    break;
+                    
+                case 6:
+                    Console.WriteLine("hatjegyű");
+                    break;
+
+
+
+
+
+                default:
+                    Console.WriteLine("hét vagy többjegyű");
+                    break;
+
+            }
+
         }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        static void feladat4()
+        {
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("feladat4");
+            Console.WriteLine();
+            Console.WriteLine("Kérek egy számot!");
+
+            double atlag = Convert.ToInt32(Console.ReadLine());
+            if (atlag > 1.4 && atlag <= 5)
+            {
+
+                Console.WriteLine("Átment");
+
+            }
+            else if (atlag > 5)
+            {
+
+                Console.WriteLine("Ilyen osztályzat nincs!");
+
+            }
+            else
+            {
+
+                Console.WriteLine("megbukott");
+
+            }
+
+
+
+
+        }
+
+
+
+
+
+        static void feladat5()
+        {
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("feladat5");
+            Console.WriteLine();
+            Console.WriteLine("Kérem az osztályzatot");
+
+            int jegy = Convert.ToInt32(Console.ReadLine());
+            switch (jegy)
+            {
+
+                case 1:
+                    Console.WriteLine("elégtelen");
+                    break;
+
+                case 2:
+                    Console.WriteLine("elégséges");
+                    break;
+
+                case 3:
+                    Console.WriteLine("közepes");
+                    break;
+
+                case 4:
+                    Console.WriteLine("jó");
+                    break;
+
+                case 5:
+                    Console.WriteLine("jeles");
+                    break;
+
+
+
+                default:
+                    Console.WriteLine("Ilyen osztályzat nincs!");
+                    break;
+            }
+
+
+
+
+
+        }
 
         static void Main(string[] args)
         {
@@ -163,8 +313,11 @@ namespace _20210927
 
             feladat3();
 
+            feladat4();
 
+            feladat5();
 
+            
 
             Console.WriteLine();
             Console.WriteLine();
